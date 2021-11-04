e6e101919677a341202618438aa0d6b9a7d162e8
Stévillis Sousa <stevillis@hotmail.com>
2021-11-03 21:11:24 -0400

    Criada view para inserir categoria

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+
+from blog.forms import CategoriaForm
 
 
-def hello_world(request):
-    return HttpResponse('<h1>Hello, world!</h1>')
+def inserir_categoria(request):
+    form = CategoriaForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'inserir_categoria.html', context)
