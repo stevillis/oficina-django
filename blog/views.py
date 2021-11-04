8c0a8a022df97aa1f7f5af38d0edccc1cbdb3e02
Stévillis Sousa <stevillis@hotmail.com>
2021-11-03 22:37:15 -0400

    Adicionada validação do formulário e persistência no Banco de Dados

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
 from blog.forms import CategoriaForm
+from blog.models import Categoria
 
 
 def inserir_categoria(request):
     form = CategoriaForm()
+    if request.method == 'POST':
+        form = CategoriaForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data['nome']
+            categoria = Categoria(nome=nome)
+            categoria.save()
+            form = CategoriaForm()
     context = {
         'form': form
     }
