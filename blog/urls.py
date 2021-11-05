e8b5735f6ef5a45eccfec5fd2ac2690b3f12f5a2
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 18:24:42 -0400

    correção da rota de visualização de post por categoria

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from blog.views import *
 urlpatterns = [
     path('', index, name='index'),
     path('<int:pk>/', visualizar_post, name='visualizar_post'),
-    path('<categoria>/', post_categoria, name='post_categoria'),
+    path('post/<categoria>/', post_categoria, name='post_categoria'),
     path('inserir_categoria/', inserir_categoria, name='inserir_categoria'),
     path('visualizar_categorias/', visualizar_categorias, name='visualizar_categorias'),
     path('editar_categoria/<int:id>', editar_categoria, name='editar_categoria'),
