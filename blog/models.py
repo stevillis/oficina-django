e281cbbf8525b8e80c5c2922fa0fa560f23514c6
Stévillis Sousa <stevillis@hotmail.com>
2021-11-05 15:22:09 -0400

    Model Post

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,17 @@ from django.db import models
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    criado_em = models.DateTimeField(auto_now_add=True)
+    alterado_em = models.DateTimeField(auto_now=True)
+    categorias = models.ManyToManyField(Categoria, related_name='posts')
+
+    def __str__(self):
+        return self.titulo
